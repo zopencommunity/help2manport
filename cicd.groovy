9d46b432e6fc1591ca914aa6ecea0903b1d541e0
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:46:57 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'help2manport'), string(name: 'DESCRIPTION', 'help2manport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'help2manport'), string(name: 'DESCRIPTION', 'help2man is a tool for automatically generating simple manual pages from program output.' )]
         }
 }
