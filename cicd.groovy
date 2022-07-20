d92b6a81bb76b080accd7060d2282d86f0c5091f
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:30 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'help2manport'), string(name: 'DESCRIPTION', 'help2man is a tool for automatically generating simple manual pages from program output.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'help2manport'), string(name: 'DESCRIPTION', value: 'help2man is a tool for automatically generating simple manual pages from program output.' )]
         }
 }
