0bc5312012febcaa3beb33c32a186b6df27a1190
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:30 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/help2manport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/help2manport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/help2manport.git'), string(name: 'PORT_DESCRIPTION', value: 'help2man is a tool for automatically generating simple manual pages from program output.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/help2manport.git'), string(name: 'PORT_DESCRIPTION', value: 'help2man is a tool for automatically generating simple manual pages from program output.' )]
         }
 }
